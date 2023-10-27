a4c826add8103518d989ad43df3392245f6101fc
Ruan <domruan333@gmail.com>
2023-10-27 15:30:40 -0300

    Botoes alterados

--- src/AplicativoEletronica/AplicativoEletronica.java
+++ src/AplicativoEletronica/AplicativoEletronica.java
@@ -51,45 +51,21 @@ public class AplicativoEletronica {
         //painel de vendas
         JPanel vendasPanel = new JPanel();
         vendasPanel.setLayout(null);
-        JButton btnVoltar = new JButton("Voltar");
-        btnVoltar.setBounds(450, 300, 100, 30);
-        btnVoltar.addActionListener(e -> {
-            cardLayout.show(content, "MAIN_PANEL");
-        });
-        vendasPanel.add(btnVoltar);
         content.add(vendasPanel, "VENDAS_PANEL");
         
         //painel de entrada
         JPanel entradaPanel = new JPanel(null);
         entradaPanel.setPreferredSize(new Dimension(600, 400));
-        JButton btnVoltarEntrada = new JButton("Voltar");
-        btnVoltarEntrada.setBounds(450, 300, 100, 30);
-        btnVoltarEntrada.addActionListener(e -> {
-            cardLayout.show(content, "MAIN_PANEL");
-        });
-        entradaPanel.add(btnVoltarEntrada);
         content.add(entradaPanel, "ENTRADA_PANEL");
         
         //painel de promocao
         JPanel promocaoPanel = new JPanel(null);
         promocaoPanel.setPreferredSize(new Dimension(600, 400));
-        JButton btnVoltarPromocao = new JButton("Voltar");
-        btnVoltarPromocao.setBounds(450, 300, 100, 30);
-        btnVoltarPromocao.addActionListener(e -> {
-            cardLayout.show(content, "MAIN_PANEL");
-        });
-        promocaoPanel.add(btnVoltarPromocao);
         content.add(promocaoPanel, "PROMOCAO_PANEL");
         
         //painel de produtos
         JPanel produtosPanel = new JPanel(null);
         produtosPanel.setPreferredSize(new Dimension(600, 400));
-        JButton btnVoltarProdutos = new JButton("Voltar");
-        btnVoltarProdutos.setBounds(450, 300, 100, 30);
-        btnVoltarProdutos.addActionListener(e -> {
-            cardLayout.show(content, "MAIN_PANEL");
-        });
-        produtosPanel.add(btnVoltarProdutos);
         content.add(produtosPanel, "PRODUTOS_PANEL");
         
         //campos de texto
@@ -108,28 +84,49 @@ public class AplicativoEletronica {
         //botões
         JButton btnAdicionar = new JButton("Adicionar");
         btnAdicionar.setBounds(350, 100, 100, 25);
-        btnAdicionar.addActionListener(e -> adicionarProduto());
+        btnAdicionar.setFont(new Font("Arial", Font.BOLD, 14));
+        btnAdicionar.setBackground(new Color(64, 64, 64));
+        btnAdicionar.setForeground(Color.WHITE);
+        btnAdicionar.setFocusPainted(false);
+        btnAdicionar.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        btnAdicionar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                adicionarProduto();
+            }
+        });
         produtosPanel.add(btnAdicionar);
 
         JButton btnEditar = new JButton("Editar");
-        btnEditar.setBounds(420, 45, 100, 25);
-        btnEditar.addActionListener(e -> editarProduto());
+        btnEditar.setBounds(350, 136, 100, 25);
+        btnEditar.setFont(new Font("Arial", Font.BOLD, 14));
+        btnEditar.setBackground(new Color(64, 64, 64));
+        btnEditar.setForeground(Color.WHITE);
+        btnEditar.setFocusPainted(false);
+        btnEditar.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        btnEditar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                editarProduto();
+            }
+        });
         produtosPanel.add(btnEditar);
 
         JButton btnRemover = new JButton("Remover");
-        btnRemover.setBounds(350, 136, 100, 25);
-        btnRemover.addActionListener(e -> removerProduto());
+        btnRemover.setBounds(350, 172, 100, 25);
+        btnRemover.setFont(new Font("Arial", Font.BOLD, 14));
+        btnRemover.setBackground(new Color(64, 64, 64));
+        btnRemover.setForeground(Color.WHITE);
+        btnRemover.setFocusPainted(false);
+        btnRemover.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        btnRemover.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                removerProduto();
+            }
+        });
         produtosPanel.add(btnRemover);
         
       //painel de clientes
         JPanel clientesPanel = new JPanel(null);
         clientesPanel.setPreferredSize(new Dimension(600, 400));
-        JButton btnVoltarClientes = new JButton("Voltar");
-        btnVoltarClientes.setBounds(450, 300, 100, 30);
-        btnVoltarClientes.addActionListener(e -> {
-            cardLayout.show(content, "MAIN_PANEL");
-        });
-        clientesPanel.add(btnVoltarClientes);
         content.add(clientesPanel, "CLIENTES_PANEL");
         
         
@@ -137,7 +134,7 @@ public class AplicativoEletronica {
         JLabel label_vendas = new JLabel("Vendas");
         label_vendas.setForeground(Color.WHITE);
         label_vendas.setFont(new Font("Yu Gothic UI", Font.BOLD, 32));
-        label_vendas.setBounds(20, 20, 116, 37);
+        label_vendas.setBounds(20, 59, 116, 37);
         label_vendas.addMouseListener(new java.awt.event.MouseAdapter() {
             @Override
             public void mouseEntered(java.awt.event.MouseEvent evt) {
@@ -161,7 +158,7 @@ public class AplicativoEletronica {
         JLabel label_entrada = new JLabel("Entrada");
         label_entrada.setForeground(Color.WHITE);
         label_entrada.setFont(new Font("Yu Gothic UI", Font.BOLD, 32));
-        label_entrada.setBounds(20, 70, 124, 40);
+        label_entrada.setBounds(20, 107, 124, 40);
         label_entrada.addMouseListener(new java.awt.event.MouseAdapter() {
         	@Override
             public void mouseEntered(java.awt.event.MouseEvent evt) {
@@ -185,7 +182,7 @@ public class AplicativoEletronica {
         JLabel label_promocoes = new JLabel("Promoções ");
         label_promocoes.setFont(new Font("Yu Gothic UI", Font.BOLD, 32));
         label_promocoes.setForeground(Color.WHITE);
-        label_promocoes.setBounds(20, 120, 176, 54);
+        label_promocoes.setBounds(20, 158, 176, 40);
         label_promocoes.addMouseListener(new java.awt.event.MouseAdapter() {
         	@Override
             public void mouseEntered(java.awt.event.MouseEvent evt) {
@@ -201,7 +198,7 @@ public class AplicativoEletronica {
         	
             @Override
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                cardLayout.show(content, "ENTRADA_PANEL");
+                cardLayout.show(content, "PROMOCOES_PANEL");
             }
         });
         sidebar.add(label_promocoes);
@@ -209,7 +206,7 @@ public class AplicativoEletronica {
         JLabel label_produtos = new JLabel("Produtos");
         label_produtos.setForeground(Color.WHITE);
         label_produtos.setFont(new Font("Yu Gothic UI", Font.BOLD, 32));
-        label_produtos.setBounds(20, 170, 140, 54);
+        label_produtos.setBounds(20, 209, 140, 54);
         label_produtos.addMouseListener(new java.awt.event.MouseAdapter() {
         	@Override
             public void mouseEntered(java.awt.event.MouseEvent evt) {
@@ -233,7 +230,7 @@ public class AplicativoEletronica {
         JLabel label_clientes = new JLabel("Clientes");
         label_clientes.setForeground(Color.WHITE);
         label_clientes.setFont(new Font("Yu Gothic UI", Font.BOLD, 32));
-        label_clientes.setBounds(20, 220, 140, 54);
+        label_clientes.setBounds(20, 268, 140, 54);
         label_clientes.addMouseListener(new java.awt.event.MouseAdapter() {
             @Override
             public void mouseEntered(java.awt.event.MouseEvent evt) {
@@ -255,9 +252,33 @@ public class AplicativoEletronica {
         sidebar.add(label_clientes);
 
         frmEletronicaPikachu.getContentPane().add(sidebar, BorderLayout.WEST);
+        
+        JLabel label_telaPrincipal = new JLabel("Tela Principal");
+        label_telaPrincipal.setForeground(Color.WHITE);
+        label_telaPrincipal.setFont(new Font("Yu Gothic UI", Font.BOLD, 30));
+        label_telaPrincipal.setBounds(10, 11, 196, 37);
+        label_telaPrincipal.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                label_telaPrincipal.setBackground(highlightColor);
+                label_telaPrincipal.setOpaque(true);
+            }
+
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                label_telaPrincipal.setBackground(defaultColor);
+                label_telaPrincipal.setOpaque(false);
+            }
+
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                cardLayout.show(content, "MAIN_PANEL");
+            }
+        });
+        sidebar.add(label_telaPrincipal);
+        
         frmEletronicaPikachu.getContentPane().add(content, BorderLayout.CENTER);
         cardLayout.show(content, "MAIN_PANEL");  
-
         frmEletronicaPikachu.setVisible(true);
     }
 
